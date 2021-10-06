234e8351db460bed46e48da03c31c2b61785c64d
Nathan <chung.nathanh@gmail.com>
2021-10-06 09:48:30 -0400

    Gets fetch request and begins base layout for assessment

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from "react";
+import "./styling/App.css";
+import AdCreativeContainer from "./container/AdCreativeContainer";
+import Header from "./components/Header";
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="Pearmill Assessment"></header>
+        <Header />
+        <AdCreativeContainer />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styling/Header.css";
+
+function Header() {
+  return (
+    <div>
+      <h1 className="Header">Pearmill Assessment</h1>
+    </div>
+  );
+}
+
+export default Header;
